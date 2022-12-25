e2f67cbf64453dbc85e9e147f0bfcb183d092ea6
ftanekha <farai.tanekha@gmail.com>
2022-12-25 21:39:29 +0000

    move images from remote to public directory

--- src/components/userFileSystemInfo/file.js
+++ src/components/userFileSystemInfo/file.js
@@ -4,9 +4,9 @@ function File ({title, type}){
     let src = ''
     //distinguish btwn regular files and folders
     if(type  === 'directory') {
-        src = `https://cdn.icon-icons.com/icons2/1379/PNG/512/folderblack_93099.png`
+        src = './directory.png'
     }else{
-        src = `https://cdn1.iconfinder.com/data/icons/file-types-29/1792/file-512.png`
+        src = '/file.png'
     }
 
     return  <img className={`${type} m-2 w-10 h-10 `} src={src} alt={title}/>
