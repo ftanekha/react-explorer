28c7f939138c714b74710463cd816cf81375cdce
ftanekha <farai.tanekha@gmail.com>
2025-03-20 11:16:16 +0000

    correc OS information toggle button title

--- src/components/toggleOsInfoAppButton.js
+++ src/components/toggleOsInfoAppButton.js
@@ -3,7 +3,7 @@ export default function ToggleOsInfoAppButton ({toggleOsDisplay}){
         <div id='toggleOsInfoAppButton'
             className='w-1/4 rollingButton bg-slate-800 border-8 border-gray-400 pt-1 text-gray-300 text-center  
                 font-medium  hover:bg-slate-700 hover:text-slate-900 transition duration-500 cursor-pointer'
-            onClick={()=> toggleOsDisplay()} title='Toggle filesystem display'
+            onClick={()=> toggleOsDisplay()} title='Toggle OS info display'
         >FS</div>
     )
 }
